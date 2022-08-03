9be28888c21eab855923321217f9c7d9ca7d1a75
Owen <nguyenduykhanh2323@gmail.com>
2022-08-03 18:57:55 +0700

    fdaf

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,3 +1,9 @@
+var mainContent = function(){
+	header = $('header').outerHeight();
+	sidebar = $('.sidebar_menu').width();
+	$('.main_content').css({'padding-top': header, 'padding-left': sidebar});
+}
+
 var menuSidebar = function()
 {
 	$(document).on('click','li.li_sidebar',function(event){
@@ -14,7 +20,41 @@ var menuSidebar = function()
 		}
 	});
 }
+var explandMiniSizeSidebar = function()
+{
+	$(document).on('click', '.button_sidebar', function(event){
+		event.preventDefault();
+		sidebar = $('.sidebar_menu');
+		content = $('.main_content');
+		if(sidebar.hasClass('show')){
+			$(this).html('<i class="bi bi-list"></i>');
+			sidebar.removeClass('show');
+			content.css('padding-left', 0);
+		}else{
+			$(this).html('<i class="bi bi-arrow-bar-left"></i>');
+			sidebar.addClass('show');
+			content.css('padding-left', 175);
+		}
+
+	});
+}
+
+var profileMenu = function()
+{
+	$(document).on('click', '.admin_menu', function(event){
+		event.preventDefault();
+		ul = $(this).find('ul.profile_menu');
+		if(ul.css('display')=='none'){
+			ul.slideDown(175);
+		}else{
+			ul.slideUp(175);
+		}
+	});
+}
 
 $(function(){
+	mainContent();
 	menuSidebar();
+	explandMiniSizeSidebar();
+	profileMenu();
 });
